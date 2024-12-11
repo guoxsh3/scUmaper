969d75f6685b9359692557b71a43951bc85f8f8b
黄金哲 <CrystalSiJerry@outlook.com>
2024-12-11 16:52:48 +0800

    初始化仓库

---
+++ R/SCAPE.R
@@ -0,0 +1,411 @@
+#' @title Single Cell Automatic Processing Engine
+#'
+#' @description 描述一下你的包在做什么事情
+#'
+#' @param input_dir 补充参数含义
+#' @param output_dir 补充参数含义
+#' @param output_plot 补充参数含义
+#' @param output_plot_format 补充参数含义
+#' @param n_features 补充参数含义
+#' @param qc_all 补充参数含义
+#' @param qc_immune 补充参数含义
+#' @param doublet_reserve 补充参数含义#'
+#'
+#' @return NULL
+#'
+#' @examples 补充使用的示例
+#'
+#' @export auto_anno
+#'
+#'
+cat("Welcome to use Single Cell Automatic Processing Engine (SCAPE) package.\n")
+cat("R version of SCAPE can be used to remove doublets and automatically annotate single cell clusters.\n\n")
+cat("SCAPE was written by Guo X. et al. from Zhoulab.\n")
+cat("All copyrights reserved. © 2024 Zhoulab.\n\n")
+
+auto_anno = function(input_dir = NULL,
+                     output_dir = NULL,
+                     output_plot = TRUE,
+                     output_plot_format = "png",
+                     n_features = 2000,
+                     qc_all = 25,
+                     qc_immune = 5,
+                     doublet_reserve = FALSE) {
+
+  require(tibble)
+  require(magrittr)
+  require(dplyr)
+  require(tidyr)
+  require(ggplot2)
+  require(Seurat)
+
+  #####reading parameters##########################################################
+  if(is.null(input_dir)) {cat("Error: `input_dir` parameter cannot be empty. Please type in a valid path.\n"); break}
+  if(!dir.exists(input_dir)) {cat("Error: Please type in a valid `input_dir` path.\n"); break}
+  if(is.null(output_dir)) {
+    dir.create(path = paste0(getwd(), "/SCAPE_output"))
+    output_dir = paste0(getwd(), "/SCAPE_output")
+  }
+  if(all(!is.null(output_dir), !dir.exists(output_dir))) {cat("Error: Please type in a valid `output_dir` path.\n"); break}
+  cat("================================================================================\n\n")
+  cat("Single cell matrices are reading from:", input_dir, "\n")
+  cat("Output files of this SCAPE run will be located in:", output_dir, "\n")
+  cat("Feature number of clustering major groups is", n_features, "\n")
+  cat("Quality control standard of all cells is less than", qc_all, "percent mitochondrial gene expression.", "\n")
+  cat("Quality control standard of immune cells is less than", qc_immune, "percent mitochondrial gene expression.", "\n")
+  if (!(output_plot_format %in% c("png", "pdf"))) {output_plot = F; cat("Warning: format", output_plot_format, "is not supported by SCAPE, only `pdf` or `png` are supported.\n")}
+  if (!output_plot) cat("UMAP plots will not be created in this run.\n")
+  if (doublet_reserve) cat("The doublets deleted will be kept in:", output_dir, "\n")
+  cat("\n", "Start running SCAPE.\n\n")
+
+  cat("================================================================================\n\n")
+  ref.df = data.frame("cell_types" = c("B/Plasma_cell", "Myeloid", "T/NK_cell", "Fibroblast", "Endothelial_cell", "Epithelial_cell"),
+                      "markers" = c("CD79A;CD79B;CD19;MS4A1;MZB1;IGHG1;IGHA1",
+                                    "CD68;CSF3R;TPSAB1",
+                                    "CD3D;CD3E",
+                                    "COL1A1;ACTA2;ACTG2;COL6A3",
+                                    "PECAM1;VWF",
+                                    "EPCAM;KRT19;KRT8"),
+                      "mt_threshold" = c(rep(qc_immune, 3), rep(qc_all, 3)))
+
+  #####read seurats##########################################################
+  input.list = list.dirs(path = input_dir, recursive = F, full.names = F) %>% unlist()
+
+  sc = NULL
+  for (each in input.list) {
+    cat("Reading", each, "\n")
+    temp = Read10X(paste(input_dir, each, sep = "/")) %>%
+      CreateSeuratObject(project = each, min.cells = 3, min.features = 200) %>%
+      RenameCells(new.names = paste(each, rownames(.@meta.data), sep = "_"))
+    temp[["percent.mt"]] = PercentageFeatureSet(temp, pattern = "^MT-")
+    temp %<>% subset(percent.mt < qc_all) %>% subset(nFeature_RNA > 200 & nFeature_RNA < 6000) ###Qualification
+    if (is.null(sc)) {
+      sc = temp
+    } else {
+      sc = merge(sc, temp)
+    }
+    rm(temp)
+    gc()
+  }
+
+  rm(input.list, input_dir, each)
+  gc()
+
+  cat("Single cell matrices are successfully loaded.\n")
+
+  #####add preliminary annotation##############################################
+  cat("================================================================================\n\n")
+  cat("Start preliminarily annotating.\n\n")
+
+  sc %<>% JoinLayers() %>%
+    NormalizeData(verbose = F) %>%
+    FindVariableFeatures(nfeatures = n_features, verbose = F) %>%
+    ScaleData(verbose = F) %>%
+    RunPCA(features = setdiff(x = VariableFeatures(.), y = grep("^MT-|^RP[SL]", VariableFeatures(.), value = T)), verbose = F) %>%
+    RunUMAP(reduction = "pca", dims = 1:20, verbose = F) %>%
+    FindNeighbors(reduction = "pca", dims = 1:20, verbose = F)
+
+  run_res = 0.05
+  repeat {
+    sc %<>% FindClusters(resolution = run_res, verbose = F)
+    if (sc@meta.data$seurat_clusters %>% levels() %>% length() < 8 & run_res < 5) {
+      run_res = run_res + 0.05
+      gc()
+      next
+    }
+
+    run_markers = FindAllMarkers(sc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, verbose = F)
+    sc$cluster = NA
+
+    for (temp.ident in levels(sc$seurat_clusters)) {
+      for (i in 1:nrow(ref.df)) {
+        temp.cluster = ref.df[i, "cell_types"]
+        temp.markers = strsplit(ref.df[i, "markers"], split = ";") %>% unlist()
+        if(any(run_markers[run_markers$cluster == temp.ident, "gene"] %in% temp.markers)) {
+          sc$cluster[sc$seurat_clusters == temp.ident] = temp.cluster
+        }
+      }
+    }
+
+    sc$cluster[is.na(sc$cluster)] = "undefined"
+
+    if (unique(sc$cluster) %>% length() < 5 & run_res < 3) {
+      run_res = run_res + 0.05
+      gc()
+      next
+    }
+
+    sc@meta.data %<>% .[,!grepl(pattern = "^RNA_snn_res|seurat_clusters", x = colnames(.))]
+    cat("Final clustering resolution for preliminary annotation is", run_res, "\n\n")
+    break
+  }
+
+  rm(list = c(ls(pattern = "^qc|^run_|^temp"), "i"))
+  gc()
+
+  cat("================================================================================\n\n")
+
+  #####remove doublets########################################################
+  cat("Start seeking for doublets.\n\n")
+
+  for (each in unique(sc$cluster)) {
+    temp = subset(sc, cluster == each)
+    gc()
+    each %<>% gsub(pattern = "/", replacement = "&", x = .) #replace '/'
+    saveRDS(temp, paste0(output_dir, "/SCAPE_temp_", each, ".rds"))
+  }
+
+  rm(temp, sc, each)
+  gc()
+
+  ref.df %<>% column_to_rownames(var = "cell_types")
+
+  sc = NULL
+  dbl = NULL
+  for (each in list.files(path = output_dir, pattern = "SCAPE_temp_.*\\.rds")) {
+    temp = readRDS(paste0(output_dir, "/", each))
+    each %<>% gsub(pattern = "SCAPE_temp_|\\.rds", replacement = "", x = .) %>% gsub(pattern = "&", replacement = "/", x = .) #replace '&'
+    cat("\nStarting find doublet of", each, "\n\n")
+
+    if (ncol(temp) < 50) { ###skip too small subclusters
+      cat(each, "has too few cells. No cells removed.\n\n")
+      if (is.null(sc)) {
+        sc = temp
+      } else {
+        sc = merge(sc, temp)
+      }
+      gc()
+      next
+    }
+
+    if (each != "undefined") temp %<>% subset(percent.mt < ref.df[each, "mt_threshold"])
+    temp %<>% NormalizeData() %>%
+      FindVariableFeatures(nfeatures = 2000, verbose = F) %>%
+      ScaleData(verbose = F) %>%
+      RunPCA(features = setdiff(x = VariableFeatures(.), y = grep("^MT-|^RP[SL]", VariableFeatures(.), value = T)), verbose = F) %>%
+      RunUMAP(reduction = "pca", dims = 1:20, verbose = F) %>%
+      FindNeighbors(reduction = "pca", dims = 1:20, verbose = F)
+
+    ###find doublets then remove
+    run_res = 1
+    repeat {
+      temp %<>% FindClusters(resolution = run_res, verbose = F)
+      gc()
+      if (temp@meta.data$seurat_clusters %>% levels() %>% length() < 15 & run_res < 5) {
+        run_res = run_res + 0.1
+        next
+      }
+
+      if (temp@meta.data$seurat_clusters %>% levels() %>% length() < 3) {
+        cat(each, "has too few cells. No cells removed.\n\n")
+        break
+      }
+
+      run_markers = FindAllMarkers(temp, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, verbose = F) %>%
+        group_by(cluster) %>%
+        slice_head(n = 50)
+
+      if (each %in% c("B/Plasma_cell", "Myeloid", "T/NK_cell")) {
+        impossb.genes = ref.df[rownames(ref.df) %>% setdiff(each), "markers"] %>%
+          paste(collapse = ";") %>%
+          strsplit(split = ";") %>%
+          unlist()
+        for (i in levels(temp$seurat_clusters)) {
+          if (any(run_markers[run_markers$cluster == i,]$gene %in% impossb.genes)) {
+            temp.dbl = subset(temp, seurat_clusters == i)
+            if (is.null(dbl)) {
+              dbl = temp.dbl
+            } else {
+              dbl = merge(dbl, temp.dbl)
+            }
+            temp %<>% subset(seurat_clusters != i) #remove doublets
+            cat("cluster", i, "of", each, "is removed.\n")
+          }
+        }
+      } else if (each %in% c("Fibroblast", "Endothelial_cell", "Epithelial_cell")){
+        impossb.genes = ref.df[rownames(ref.df) %>% setdiff(each), "markers"] %>%
+          paste(collapse = ";") %>%
+          strsplit(split = ";") %>%
+          unlist() %>%
+          c(., "PTPRC")
+        for (i in levels(temp$seurat_clusters)) {
+          if (any(run_markers[run_markers$cluster == i,]$gene %in% impossb.genes)) {
+            temp.dbl = subset(temp, seurat_clusters == i)
+            if (is.null(dbl)) {
+              dbl = temp.dbl
+            } else {
+              dbl = merge(dbl, temp.dbl)
+            }
+            temp %<>% subset(seurat_clusters != i) #remove doublets
+            cat("cluster", i, "of", each, "is removed.\n")
+          }
+        }
+      } else {
+        impossb.genes = ref.df[c("B/Plasma_cell", "Myeloid", "T/NK_cell"), "markers"] %>%
+          paste(collapse = ";") %>%
+          strsplit(split = ";") %>%
+          unlist() %>%
+          c(., "PTPRC")
+        impossb.genes2 = ref.df[c("Fibroblast", "Endothelial_cell", "Epithelial_cell"), "markers"] %>%
+          paste(collapse = ";") %>%
+          strsplit(split = ";") %>%
+          unlist()
+        for (i in levels(temp$seurat_clusters)) {
+          if (any(run_markers[run_markers$cluster == i,]$gene %in% impossb.genes) & any(run_markers[run_markers$cluster == i,]$gene %in% impossb.genes2)) {
+            temp.dbl = subset(temp, seurat_clusters == i)
+            if (is.null(dbl)) {
+              dbl = temp.dbl
+            } else {
+              dbl = merge(dbl, temp.dbl)
+            }
+            temp %<>% subset(seurat_clusters != i) #remove doublets
+            cat("cluster", i, "of", each, "is removed.\n")
+          }
+        }
+      }
+
+      cat("\nFinal clustering resolution of", each, "is", run_res, "\n\n")
+      break
+    }
+
+    if (is.null(sc)) {
+      sc = temp
+    } else {
+      sc = merge(sc, temp)
+    }
+    gc()
+  }
+
+  rm(list = c(ls(pattern = "^temp|^run_"), "each", "i", "impossb.genes"))
+  gc()
+
+  if (!is.null(dbl)) {
+    dbl %<>% JoinLayers()
+    cat("\nTotally", ncol(dbl), "possible doublet cells have been removed.\n")
+    if (doublet_reserve) {
+      saveRDS(dbl, paste0(output_dir, "/SCAPE_removed_doublet_seurat.rds"))
+      cat(paste("Removed doublets are saved at:", paste0(output_dir, "/final_removed_doublet_seurat.rds")), "\n\n")
+    }
+  } else {
+    cat("No doublet was found or removed.\n\n")
+  }
+
+  rm(dbl)
+  gc()
+
+  #####get final results########################################################
+  cat("================================================================================\n\n")
+  cat("Start final annotating.\n\n")
+
+  sc %<>% JoinLayers() %>%
+    NormalizeData(verbose = F) %>%
+    FindVariableFeatures(nfeatures = n_features, verbose = F) %>%
+    ScaleData(verbose = F) %>%
+    RunPCA(features = setdiff(x = VariableFeatures(.), y = grep("^MT-|^RP[SL]", VariableFeatures(.), value = T)), verbose = F) %>%
+    RunUMAP(reduction = "pca", dims = 1:20, verbose = F) %>%
+    FindNeighbors(reduction = "pca", dims = 1:20, verbose = F)
+  gc()
+
+  ref.df %<>% rownames_to_column(var = "cell_types")
+  run_res = 0.05
+  repeat {
+    sc %<>% FindClusters(resolution = run_res, verbose = F)
+    if (sc@meta.data$seurat_clusters %>% levels() %>% length() < 8 & run_res < 5) {
+      run_res = run_res + 0.05
+      next
+    }
+
+    run_markers = FindAllMarkers(sc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, verbose = F)
+    sc$cluster = NA
+
+    for (temp.ident in levels(sc$seurat_clusters)) {
+      for (i in 1:nrow(ref.df)) {
+        temp.cluster = ref.df[i, "cell_types"]
+        temp.markers = strsplit(ref.df[i, "markers"], split = ";") %>% unlist()
+        if(any(run_markers[run_markers$cluster == temp.ident, "gene"] %in% temp.markers)) {
+          sc$cluster[sc$seurat_clusters == temp.ident] = temp.cluster
+        }
+      }
+    }
+
+    sc$cluster[is.na(sc$cluster)] = "undefined"
+
+    if (unique(sc$cluster) %>% length() < 5 & run_res < 3) {
+      run_res = run_res + 0.05
+      next
+    }
+
+    sc@meta.data %<>% .[,!grepl(pattern = "^RNA_snn_res|seurat_clusters", x = colnames(.))]
+    cat("\nLast clustering resolution for final annotation is", run_res, "\n\n")
+    cat("================================================================================\n\n")
+    break
+  }
+
+  sc$cluster %<>% factor(levels = c("T/NK_cell", "B/Plasma_cell", "Myeloid", "Fibroblast", "Endothelial_cell", "Epithelial_cell", "undefined"))
+  Idents(sc) = sc$cluster
+
+  file.remove(list.files(path = output_dir, pattern = "SCAPE_temp_.*\\.rds", full.names = T))
+  rm(list = c(ls(pattern = "^run_|^temp"), "i"))
+  gc()
+
+  saveRDS(sc, paste0(output_dir, "/SCAPE_annotated_singlet_seurat.rds"))
+  cat("\nEventually,", ncol(sc), "singlet cells have been kept and annotated.\n")
+  cat("Final processed seurat object has been successfully saved as:\n")
+  cat(output_dir, "/final_annotated_singlet_seurat.rds\n\n", sep = "")
+
+  if (output_plot) {
+    dir.create(path = paste0(output_dir, "/markers_plot"))
+    if (output_plot_format == "pdf") {
+      temp.p = DimPlot(sc,
+                       reduction = "umap",
+                       group.by = "cluster",
+                       label = F,
+                       pt.size = .1, raster = F,
+                       cols = c("#E56F5E", "#F6C957", "#FBE8D5", "#ABD0F1", "#79CEED", "#43978F", "#B5B5B5"))
+      pdf(paste0(output_dir, "/final_umap.pdf"), height = 6, width = 7.5)
+      print(temp.p)
+      dev.off()
+
+      for (each in c("PTPRC", "CD3E", "CD79A", "MS4A1", "MZB1", "IGHG1", "IGHA1", "CD68", "CSF3R",
+                     "EPCAM", "PECAM1", "COL1A1", "ACTA2")) {
+        temp.p = FeaturePlot(sc,
+                             features = each,
+                             cols = c("lightgrey","red"),
+                             pt.size = 0.1,
+                             raster = F,
+                             ncol = 1)
+        pdf(paste0(output_dir, "/markers_plot/", each, "_umap.pdf"), height = 6, width = 6.5)
+        print(temp.p)
+        dev.off()
+      }
+    } else if(output_plot_format == "png") {
+      temp.p = DimPlot(sc,
+                       reduction = "umap",
+                       group.by = "cluster",
+                       label = F,
+                       pt.size = .1, raster = F,
+                       cols = c("#E56F5E", "#F6C957", "#FBE8D5", "#ABD0F1", "#79CEED", "#43978F", "#B5B5B5"))
+      ggsave(filename = paste0(output_dir, "/final_umap.png"), plot = temp.p, height = 6, width = 7.5)
+
+      for (each in c("PTPRC", "CD3E", "CD79A", "MS4A1", "MZB1", "IGHG1", "IGHA1", "CD68", "CSF3R",
+                     "EPCAM", "PECAM1", "COL1A1", "ACTA2")) {
+        temp.p = FeaturePlot(sc,
+                             features = each,
+                             cols = c("lightgrey","red"),
+                             pt.size = 0.1,
+                             raster = F,
+                             ncol = 1)
+        ggsave(paste0(output_dir, "/markers_plot/", each, "_umap.png"), plot = temp.p, height = 6, width = 6.5)
+      }
+    }
+    cat("Final umaps have been successfully saved at:", paste0(output_dir, "/markers_plot"), "\n\n")
+  }
+
+  rm(list = ls())
+  gc()
+
+  cat("================================================================================\n\n")
+  cat("All progressions have been completed. Thank you for using SCAPE.\n\n")
+  cat("Please cite SCAPE while publishing your papers.\n")
+  cat("© 2024 Zhoulab. All rights reserved.\n")
+}
